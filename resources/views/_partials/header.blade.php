f5b363d04e4e9076507a4fe57db07756bdb1c8a4
MantovaniWebDeveloper <ratedsuperstar84@gmail.com>
2019-03-06 15:42:12 +0100

    header finito

--- resources/views/_partials/header.blade.php
+++ resources/views/_partials/header.blade.php
@@ -4,7 +4,11 @@
       <h2 class="title">{{config('app.name')}}</h2>
     </div>
     <div class="wrapMenu">
-
+        <ul>
+          <a href="#"><li class="item">Home</li></a>
+          <a href="#"><li class="item">Bio</li></a>
+          <a href="#"><li class="item">Contact</li></a>
+        </ul>
     </div>
   </div>
 </header>
