0c86d764ef81e151d459640c09a7aa9b7dba0c6a
MantovaniWebDeveloper <ratedsuperstar84@gmail.com>
2019-03-06 15:36:36 +0100

    configurato header con app.name ed includo header in index

--- resources/views/_partials/header.blade.php
+++ resources/views/_partials/header.blade.php
@@ -1,7 +1,7 @@
 <header>
-  <div class="container">
+  <div id="contenitore_logo_menu" class="container">
     <div class="wrapLogo">
-
+      <h2 class="title">{{config('app.name')}}</h2>
     </div>
     <div class="wrapMenu">
 
